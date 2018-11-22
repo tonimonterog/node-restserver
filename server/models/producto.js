1cf8a3c68bc8e02eb64192522c0c3803421ee536
Antonio <tonimonterog@gmail.com>
2018-11-22 12:56:46 +0100

    Subida de archivos

--- server/models/producto.js
+++ server/models/producto.js
@@ -20,6 +20,10 @@ var productoSchema = new Schema({
         required: true,
         default: true
     },
+    img: {
+        type: String,
+        required: false,
+    },
     categoria: {
         type: Schema.Types.ObjectId,
         ref: 'Categoria',
